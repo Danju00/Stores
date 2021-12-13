8a9e33396e46a211a84be12f2859d3d9f452948b
Danju00 <leinad9265@gmail.com>
2021-12-13 02:00:41 -0600

    138. Menú en Fragment

--- app/src/main/java/com/cursosant/android/stores/EditStoreFragment.kt
+++ app/src/main/java/com/cursosant/android/stores/EditStoreFragment.kt
@@ -1,10 +1,8 @@
 package com.cursosant.android.stores
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.cursosant.android.stores.databinding.FragmentEditStoreBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -31,6 +29,12 @@ class EditStoreFragment : Fragment() {
         activity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
         activity?.supportActionBar?.title= getString(R.string.edit_store_title_add)
 
+        setHasOptionsMenu(true)
+
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_save, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
 }
