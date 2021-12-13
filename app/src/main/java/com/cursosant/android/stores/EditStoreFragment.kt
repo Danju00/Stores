789c61661fda0d75794d66995d9ead24541916c3
Danju00 <leinad9265@gmail.com>
2021-12-13 02:13:59 -0600

    139. Selección de un menú

--- app/src/main/java/com/cursosant/android/stores/EditStoreFragment.kt
+++ app/src/main/java/com/cursosant/android/stores/EditStoreFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import com.cursosant.android.stores.databinding.FragmentEditStoreBinding
+import com.google.android.material.snackbar.Snackbar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,6 +14,7 @@ private const val ARG_PARAM2 = "param2"
 
 class EditStoreFragment : Fragment() {
     private lateinit var mBinding: FragmentEditStoreBinding
+    private var mActivity: MainActivity? = null
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -25,9 +27,9 @@ class EditStoreFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val activity= activity as? MainActivity
-        activity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        activity?.supportActionBar?.title= getString(R.string.edit_store_title_add)
+        mActivity= activity as? MainActivity
+        mActivity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mActivity?.supportActionBar?.title= getString(R.string.edit_store_title_add)
 
         setHasOptionsMenu(true)
 
@@ -36,5 +38,23 @@ class EditStoreFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_save, menu)
         super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                mActivity.onBackPressed()
+                true
+
+            }
+            R.id.action_save -> {
+                Snackbar.make(mBinding.root, getString(R.string.edit_store_message_save_success),
+                    Snackbar.LENGTH_SHORT)
+                    .show()
+                true
+            }
+            else-> return super.onOptionsItemSelected(item)
+        }
+       // return super.onOptionsItemSelected(item)
     }
 }
