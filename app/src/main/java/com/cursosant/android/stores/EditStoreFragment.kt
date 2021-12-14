3c3af580a9dee501ae8ab89165b22117915bcfb7
Danju00 <leinad9265@gmail.com>
2021-12-14 01:10:09 -0600

    151. Actualizar registro desde Fragment

--- app/src/main/java/com/cursosant/android/stores/EditStoreFragment.kt
+++ app/src/main/java/com/cursosant/android/stores/EditStoreFragment.kt
@@ -39,7 +39,8 @@ class EditStoreFragment : Fragment() {
             mIsEditMode = true
             getStore(id)
         } else{
-            Toast.makeText(activity, id.toString(), Toast.LENGTH_SHORT).show()
+           mIsEditMode = false
+            mStoreEntity = StoreEntity(name = "", phone = "", photoUrl = "")
         }
 
         mActivity= activity as? MainActivity
@@ -92,32 +93,52 @@ class EditStoreFragment : Fragment() {
 
             }
             R.id.action_save -> {
-                val store = StoreEntity(name = mBinding.etName.text.toString().trim(),
-                    phone = mBinding.etPhone.text.toString().trim(),
-                    website= mBinding.etWebsite.text.toString().trim(),
-                photoUrl = mBinding.etPhotoUrl.text.toString().trim())
-
-                doAsync {
-                    store.id= StoreApplication.database.storeDao().addAllStore(store)
-                    uiThread {
-                        mActivity.addStore(store)
-
-                        hideKeyboard()
-
-//                        Snackbar.make(mBinding.root, getString(R.string.edit_store_message_save_success),
-//                            getString(R.string.edit_store_message_save_success)
-//                                    Snackbar.LENGTH_SHORT)
-//                            .show()
-                        Toast.makeText(mActivity, R.string.edit_store_message_save_success, Toast.LENGTH_SHORT).show()
-
-                        mActivity.onBackPressed()
+                if (mStoreEntity != null) {
+//                val store = StoreEntity(name = mBinding.etName.text.toString().trim(),
+//                    phone = mBinding.etPhone.text.toString().trim(),
+//                    website= mBinding.etWebsite.text.toString().trim(),
+//                photoUrl = mBinding.etPhotoUrl.text.toString().trim())
+                    with(mStoreEntity!!) {
+                        name = mBinding.etName.text.toString().trim()
+                        phone = mBinding.etPhone.text.toString().trim()
+                        website = mBinding.etWebsite.text.toString().trim()
+                        photoUrl = mBinding.etPhotoUrl.text.toString().trim()
                     }
-                }
 
-                Snackbar.make(mBinding.root, getString(R.string.edit_store_message_save_success),
-                    getString(R.string.edit_store_message_save_success)
-                    Snackbar.LENGTH_SHORT)
-                    .show()
+                    doAsync {
+                        if (mIsEditMode) StoreApplication.database.storeDao().updateStore(mStoreEntity!!)
+                        else  mStoreEntity!!.id = StoreApplication.database.storeDao().addAllStore(mStoreEntity!!)
+
+                        uiThread {
+                            hideKeyboard()
+                            if (mIsEditMode){
+                                mActivity?.updateStore(mStoreEntity!!)
+
+                                Snackbar.make(mBinding.root,
+                                    R.string.edit_store_message_update_success,
+                                    Snackbar.LENGTH_SHORT.show())
+                            } else {
+                                mActivity?.addStore(mStoreEntity!!)
+
+
+
+                        Snackbar.make(mBinding.root, getString(R.string.edit_store_message_save_success),
+                           getString(R.string.edit_store_message_save_success)
+                                  Snackbar.LENGTH_SHORT)
+                              .show()
+                                Toast.makeText(
+                                    mActivity,
+                                    R.string.edit_store_message_save_success,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                mActivity?.onBackPressed()
+                            }
+                        }
+                    }
+
+
+                }
                 true
             }
             else-> return super.onOptionsItemSelected(item)
--- app/src/main/java/com/cursosant/android/stores/MainActivity.kt
+++ app/src/main/java/com/cursosant/android/stores/MainActivity.kt
@@ -115,6 +115,7 @@ class MainActivity : AppCompatActivity(), OnClickListener, MainAux {
     }
 
     override fun updateStore(storeEntity: StoreEntity) {
+        mAdapter.update(storeEntity)
 
     }
 }
